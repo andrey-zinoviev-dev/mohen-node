f0a7b6672c46a6588b7d2588002fd9d062a2b1fb
Andrey Zinoviev <dronis_dronis@mail.ru>
2024-10-03 18:17:12 +0300

    test router

--- routers/goodsRouter.js
+++ routers/goodsRouter.js
@@ -5,7 +5,9 @@ const goodsRouter = express();
 const { auth } = require("../middlewares/authMiddlewares");
 
 const { showGoods, showAccountGoods, showGood, addGood, updateBatch } = require("../controllers/Goods");
-
+goodsRouter.get("/test", (req, res) => {
+    return res.status(200).send({message: "Тестовое сообщение"});
+})
 goodsRouter.get("/showGoods", showGoods);
 goodsRouter.get("/showGoods/user", auth, showAccountGoods)
 goodsRouter.get("/showGoods/:id", showGood);
