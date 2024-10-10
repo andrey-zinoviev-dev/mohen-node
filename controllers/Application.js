e3d16f5157e7484882cf52b412d317e0a55cef86
Andrey <55049581+andrey-zinoviev-dev@users.noreply.github.com>
2024-10-10 04:06:16 +0300

    updated application model and controller

--- controllers/Application.js
+++ controllers/Application.js
@@ -6,16 +6,16 @@ const Users = require("../models/user");
 const dotenv = require("dotenv");
 dotenv.config();
 //s3
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-const s3ClientProfile = new S3Client({
-  region: process.env.AWS_REGION,
-  endpoint: process.env.AWS_ENDPOINT,
-  credentials: {
-    accessKeyId: process.env.AWS_KEY_ACCESS,
-    secretAccessKey: process.env.AWS_KEY_SECRET,
-  }
-})
+// const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+// const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+// const s3ClientProfile = new S3Client({
+//   region: process.env.AWS_REGION,
+//   endpoint: process.env.AWS_ENDPOINT,
+//   credentials: {
+//     accessKeyId: process.env.AWS_KEY_ACCESS,
+//     secretAccessKey: process.env.AWS_KEY_SECRET,
+//   }
+// })
 
 
 const getApplication = (req, res) => {
@@ -26,40 +26,40 @@ const getApplication = (req, res) => {
     if(!doc) {
       throw new Error("Анкета не найдена");
     }
-    Promise.all(doc.photos.value.map((photo) => {
-      const readCommand = new GetObjectCommand({
-        Bucket: process.env.AWS_NAME,
-        Key: photo.name,
-      });
-      return getSignedUrl(s3ClientProfile, readCommand)
-      .then((url) => {
-        // console.log(url);
-        photo.path = url;
-        return photo;
-      })
-    }))
-    .then((data) => {
-      return res.status(201).send(doc);
-    })
-    // return res.status(201).send(doc);
+    return res.status(200).send(JSON.stringify(doc));
   })
 };
 
 const sendApplication = (req, res) => {
-  Applications.findOne({phone: req.body.phone.value})
+  Applications.findOne({phone: req.body.phone})
   .then((doc) => {
     // console.log(doc);
     if(doc) {
       throw new Error("Анкета уже была отправлена");
     } 
-    // console.log(req.body.photos);
-    Applications.create(req.body)
+    const { application } = req.body;
+    // console.log(req.body);
+    Applications.create({
+      name: application.name,
+      email: application.email,
+      phone: application.phone,
+      city: application.city,
+      category: application.category,
+      description: application.description,
+      productionLength: application.productionLength,
+      productionProcess: application.productionProcess,
+      stock: application.stock,
+      size: application.size,
+      dateOfFill: application.dateOfFill,
+      photos: application.photos.map((photo) => {
+        return {...photo, url: `http://cdn.mohen-tohen.ru/${photo.title}`}
+      }),
+      approved: application.approved,
+    })
     .then((createdDoc) => {
       return res.status(201).send(createdDoc);
     })
   })
-  // console.log(req.body);
-  // res.status(201).send({message: "no"});
 };
 
 const decideApplication = (req, res) => {
@@ -71,21 +71,11 @@ const decideApplication = (req, res) => {
     if(!doc) {
       throw new Error("Анкета не найдена");
     }
-    // console.log(decision);
-    doc.approved = {value: decision, approved: true};
 
-    // // doc.approved = decision;
+    doc.approved = decision;
     doc.save();
 
-    if(decision) {
-      return Users.create({name: doc.name.value, description: doc.description.value, cover: "", goodsCollection: [], seller: true, phone: doc.phone.value})
-      .then((createdBrand) => {
-        return res.status(201).send(doc);
-      })
-
-    }
-    
-    return res.status(201).send(doc);
+    return res.status(201).send(JSON.stringify({decision: decision}));
   })
 };
 
--- models/applications.js
+++ models/applications.js
@@ -1,47 +1,63 @@
 const mongoose = require("mongoose");
 
 const photoSchema = new mongoose.Schema({
-  name: String,
-  type: String,
-  path: String,
+  title: String,
+  // type: String,
+  url: String,
 });
 
 // const 
 
+// const applicationSchema = new mongoose.Schema({
+//   name: {value: String, textarea: Boolean, label: String},
+//   email: {value: String, textarea: Boolean, label: String},
+//   phone: {value: String, textarea: Boolean, label: String},
+//   city: {value: String, textarea: Boolean, label: String},
+//   category: {value: [String], textarea: Boolean, label: String},
+//   description: {value: String, textarea: Boolean, label: String},
+//   productionLength: {value: String, textarea: Boolean, label: String},
+//   productionProcess: {value: String, textarea: Boolean, label: String},
+//   stock: {value: String, textarea: Boolean, label: String},
+//   size: {value: String, textarea: Boolean, label: String},
+//   dateOfFill: {value: String, textarea: Boolean, date: Boolean},
+//   photos: {
+//     value: [photoSchema],
+//     photo: Boolean,
+//   },
+//   approved: {
+//     value: {
+//       approved: Boolean,
+//       declined: Boolean,
+//       // default: {
+//       //   approved: false,
+//       //   declined: false,
+//       // }
+//     }, 
+//     approved: Boolean,
+//     // default: {
+//     //   value: {
+//     //     approved: false,
+//     //     declined: false,
+//     //   },
+//     //   approved: false,
+//     // },
+//   },
+// });
+
 const applicationSchema = new mongoose.Schema({
-  name: {value: String, textarea: Boolean, label: String},
-  email: {value: String, textarea: Boolean, label: String},
-  phone: {value: String, textarea: Boolean, label: String},
-  city: {value: String, textarea: Boolean, label: String},
-  category: {value: [String], textarea: Boolean, label: String},
-  description: {value: String, textarea: Boolean, label: String},
-  productionLength: {value: String, textarea: Boolean, label: String},
-  productionProcess: {value: String, textarea: Boolean, label: String},
-  stock: {value: String, textarea: Boolean, label: String},
-  size: {value: String, textarea: Boolean, label: String},
-  dateOfFill: {value: String, textarea: Boolean, date: Boolean},
-  photos: {
-    value: [photoSchema],
-    photo: Boolean,
-  },
-  approved: {
-    value: {
-      approved: Boolean,
-      declined: Boolean,
-      // default: {
-      //   approved: false,
-      //   declined: false,
-      // }
-    }, 
-    approved: Boolean,
-    // default: {
-    //   value: {
-    //     approved: false,
-    //     declined: false,
-    //   },
-    //   approved: false,
-    // },
-  },
-});
+  name: String,
+  email: String,
+  phone: String,
+  city: String,
+  category: [String],
+  description: String,
+  productionLength: String,
+  productionProcess: String,
+  stock: String,
+  size: String,
+  dateOfFill: String,
+  photos: [photoSchema],
+  approved: String,
+})
 
 module.exports = mongoose.model("Applications", applicationSchema);
